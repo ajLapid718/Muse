5597d3e06f323403952c6657face0e951e11b856
Allan James Lapid <ajLapid718@gmail.com>
2017-09-09 15:10:01 -0400

    Rails generate migration add_user_id_to_post and migrate any pending migrations to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909190734) do
+ActiveRecord::Schema.define(version: 20170909190913) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170909190734) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
