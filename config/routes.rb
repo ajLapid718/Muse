740b1aedb5876e2791e46b8911e1bc038bcf2cf0
Allan James Lapid <ajLapid718@gmail.com>
2017-09-09 20:50:30 -0400

    Change put to get

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   resources :posts do
     member do
-      put "like", to: "posts#upvote"
-      put "like", to: "posts#downvote"
+      get "like", to: "posts#upvote"
+      get "like", to: "posts#downvote"
     end
   end
 
